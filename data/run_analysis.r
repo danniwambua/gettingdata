521cf8cbe995e56002ee31463c36979ed35a1c87
Daniel Maingi <66689622+danniwambua@users.noreply.github.com>
2020-09-25 16:02:57 +0300

    Update run_analysis.r
    final submission

--- data/run_analysis.r
+++ data/run_analysis.r
@@ -17,10 +17,12 @@ setwd("./data")
 activitylabel<-read.table("UCI HAR Dataset/activity_labels.txt",col.names=c("activity", "activitydescription"))
 features<-read.table("UCI HAR Dataset/features.txt")
 testvariables<-data.table(read.table("UCI HAR Dataset/test/X_test.txt"))
+names(testvariables)<-features$V2
 testactivity<-read.table("UCI HAR Dataset/test/y_test.txt")
 testsubject<-read.table("UCI HAR Dataset/test/subject_test.txt")
 testdata<-testsubject %>% mutate(subject ="candidate",.before=NULL)%>% cbind(testvariables) %>% cbind(testactivity)    
 trainvariables<-data.table(read.table("UCI HAR Dataset/train/X_train.txt"))
+names(trainvariables)<-features$V2
 trainactivity<-read.table("UCI HAR Dataset/train/y_train.txt")
 trainsubject<-read.table("UCI HAR Dataset/train/subject_train.txt")
 traindata<-trainsubject  %>% mutate(subject ="trainer",.before=NULL) %>% cbind(trainvariables) %>% cbind(trainactivity) 
@@ -28,17 +30,20 @@ traindata<-trainsubject  %>% mutate(subject ="trainer",.before=NULL) %>% cbind(t
 #Merges the training and the test sets to create one data set.
 mergedvariables<-rbind(testdata,traindata)
 
-#Extracts only the measurements on the mean and standard deviation for each measurement.
-meanstd<-mergedvariables[,c(1,2,3:8,564)]
-
-
 #Appropriately labels the data set with descriptive variable names.
-activitynames<-c("id","subject","BodyAcceleration_meanX","BodyAcceleration_meanY","BodyAcceleration_meanZ","BodyAcceleration_stdX","BodyAcceleration_stdY","BodyAcceleration_stdZ","activity")
-names(meanstd)<-(activitynames)
+meanstd1<-mergedvariables[,c(1,2,564)]
+activitynames<-c("id","subject","activity")
+names(meanstd1)<-(activitynames)
+
+#Extracts only the measurements on the mean and standard deviation for each measurement.
+meanstd2<-mergedvariables%>% select(contains(c("mean","std")))
+meanstd<-cbind(meanstd1,meanstd2)
+ 
 
 #Uses descriptive activity names to name the activities in the data set
 for(i in 1:6){meanstd["activity"][meanstd["activity"]==i]<-activitylabel[i,]$activitydescription}
 
+
 #From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 data.table(meanstd)
-meanstd %>% group_by(subject,id,activity) %>% summarise_at(c("meanX","meanY","meanZ","stdX","stdY","stdZ"),mean)
+meanstd %>% group_by(subject,id,activity) %>% summarise(across(contains("mean"),mean)) 
